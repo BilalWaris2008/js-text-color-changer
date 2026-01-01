ec08f147c2cf7f83c76e6e2cc79f8340ca0e9b69
M.Bilal Waris <bilalwaris543@gmail.com>
2026-01-01 16:20:39 +0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+function btn1(){
+    document.getElementById("one").style.color = "blue"
+}
+
+function btn2(){
+    document.getElementById("two").style.color = "red"
+}
+
+function btn3(){
+    document.getElementById("three").style.color = "green"
+}
+
+function btn4(){
+    document.getElementById("four").style.color = "aqua"
+}
+
+function btn5(){
+    document.getElementById("five").style.color = "purple"
+}
+
+function btn6(){
+    document.getElementById("six").style.color = "brown"
+}
+
+function reset(){
+    document.getElementById("one").style.color = "black"
+    document.getElementById("two").style.color = "black"
+    document.getElementById("three").style.color = "black"
+    document.getElementById("four").style.color = "black"
+    document.getElementById("five").style.color = "black"
+    document.getElementById("six").style.color = "black"
+}
